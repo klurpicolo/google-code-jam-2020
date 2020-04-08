4e2b4e9a32efd76c8b6d26780059dd79c64d5d72
warit <warit.boonmasiri@gmail.com>
2020-04-08 23:53:59 +0700

    Initial commit

---
+++ src/codeJam2020/qualification/nestingdepth/NestingDepth.java
@@ -0,0 +1,73 @@
+package codeJam2020.qualification.nestingdepth;
+
+import java.util.*;
+import java.io.*;
+
+/**
+ * In
+ * 4
+ * 0000
+ * 101
+ * 111000
+ * 1
+ * Out
+ * Case #1: 0000
+ * Case #2: (1)0(1)
+ * Case #3: (111)000
+ * Case #4: (1)
+ *
+ */
+public class NestingDepth {
+
+    private static final char LEFT = '(';
+    private static final char RIGHT = ')';
+
+    public static void solve(String input){
+//        System.out.println("Input : "+ input);
+        StringBuilder sb = new StringBuilder();
+        char[] numbers = input.toCharArray();
+
+        for(int count = 0; count<numbers.length; count++){
+            int number = Character.getNumericValue(numbers[count]);
+            if(count==0){
+                //most left
+                sb.append(getCharNum(LEFT,number));
+                sb.append(number);
+            } else {
+                int prev = Character.getNumericValue(numbers[count-1]);
+                int diff = number - prev;
+                if(diff>0){
+                    sb.append(getCharNum(LEFT,diff));
+                }
+                if(diff<0){
+                    sb.append(getCharNum(RIGHT, Math.abs(diff)));
+                }
+                sb.append(number);
+            }
+            if(count==numbers.length-1) {
+                //most right
+                sb.append(getCharNum(RIGHT, number));
+            }
+        }
+        System.out.println(sb.toString());
+
+    }
+
+    private static String getCharNum(char c, int n){
+        StringBuilder sb = new StringBuilder();
+        for (int i=1;i<=n;i++){
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+//        Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+        Scanner sc = new Scanner(new FileInputStream("D:/03_Temp/google-code-jam/src/codeJam2020/qualification/nestingdepth/test_data.txt"));
+        int testCase = sc.nextInt();
+        for (int testCount = 1; testCount<=testCase;testCount++){
+            System.out.print("Case #" + testCount + ": ");
+            solve(sc.next());
+        }
+    }
+}
